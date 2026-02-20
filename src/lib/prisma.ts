f783fae1268ec1d84ed05728f5cac6acb14ca8f6
Werachai Nachiangmai <ae1nt14@gmail.com>
2026-02-20 17:43:27 +0700

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,14 @@
+import { Pool } from 'pg'
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
-
-const connectionString = process.env.DATABASE_URL!
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 export const prisma = globalForPrisma.prisma ?? (() => {
-  const adapter = new PrismaPg(connectionString)
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+  const adapter = new PrismaPg(pool)
   return new PrismaClient({ adapter })
 })()
 
